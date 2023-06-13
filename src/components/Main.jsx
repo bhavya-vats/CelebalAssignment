9639c2e06bde41e0a5f5db7596154a18eee68419
Bhavya <bhavya.vats05@gmail.com>
2023-06-13 14:27:07 +0530

    new

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,18 +7,15 @@ function Main() {
     
     <div className='row'>
         <Header/>
-      <div>
+      <div className='row1'>
         <p className='main-content'>Bilgi Teknolojilerinde <br/>23 Yıllık Tecrübe</p>
         <p className='subject'><br></br>Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.</p>
         <p><br></br></p>
-        <div>
+        <div className='inputrow'>
             <input type="text" placeholder="Mail bültenimize kayıt ol" className='textinput'/>
             <button className='btn'><a className='link' href='#'>Kayıt Ol</a></button>
       </div>
-
-      
     </div>
-
     </div>
   )
 }
