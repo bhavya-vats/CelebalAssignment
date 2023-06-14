f9796492e6d73d0a2936ac88b1860fa70af4c31d
Bhavya <bhavya.vats05@gmail.com>
2023-06-14 14:33:21 +0530

    new

--- src/components/SubBody.jsx
+++ src/components/SubBody.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './SubBody.css';
+import SubImg from '../assets/Subbodyimg.png';
 
 function SubBody() {
   return (
-    <div>
+    <div className='container2'>
       <div>
-        
+        <img src={SubImg} alt='href' className='subImg'/>
       </div>
     </div>
   )
