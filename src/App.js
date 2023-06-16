f6135b68d0d527c7b4285adb3ec4be4283bf130f
Bhavya <bhavya.vats05@gmail.com>
2023-06-16 16:24:37 +0530

    new

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SubBody from './components/SubBody';
 import SubBody2 from './components/SubBody2';
 import SubBody3 from './components/SubBody3';
 import SubBody4 from './components/SubBody4';
+import SubBody5 from './components/SubBody5';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
     <SubBody2/>
     <SubBody3/>
     <SubBody4/>
+    <SubBody5/>
    </div>
   );
 }
---
+++ src/components/SubBody5.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './SubBody5.css'
+
+function SubBody5() {
+  return (
+    <>
+    <div className='bgcolor'>
+        <div>
+        <p className='text1'>Çözüm ve Hizmetler</p>
+      <p className='text2'>Yazılım Geliştirme</p>
+      <p className='text3'>Outsourcing</p>
+      <p className='text4'>Kalite ve Süreç Yönetimi</p>
+      <p className='text5'>Danışmanlık</p>
+      <p className='text6'>IoT Destekli Çözümler</p>
+      <p className='text7'>© Copyright 2010-2021 - Can Çevik</p>
+      <p className='text8'>Ürünler</p>
+      <p className='text9'>Eğitim Yönetim Sistemi</p>
+      <p className='text10'>İnsan Sermayesi Yönetim Sistemi</p>
+      <p className='text11'>Müşteri İlişkileri Yönetim Sistemi</p>
+      <p className='text12'>İçerik Yönetim Sistemi</p>
+      <p className='text13'>Kurumsal</p>
+      <p className='text14'>Hakkımızda</p>
+      <p className='text15'>Belge ve Yetkinlikler</p>
+      <p className='text16'>İş Ortakları</p>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default SubBody5
